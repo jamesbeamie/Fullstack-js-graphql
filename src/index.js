7c5c29b8c7df248c3bd6a079d14a4a381257a381
james wafula <jameswafula@jamess-MacBook-Pro-2.local>
2019-04-22 23:51:19 +0300

    Reg and login

--- src/index.js
+++ src/index.js
@@ -1,53 +1,28 @@
 const { GraphQLServer} = require('graphql-yoga');
-const { prisma } = require('./generated/prisma-client'); 
+const { prisma } = require('./generated/prisma-client');
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link') 
 
-// const typeDefs = `
-// type Query {
-//     info: String!
-//     feed: [Link!]!
-// }
 
-// type Mutation{
-//     post(url: String!, description: String!): Link!
-// }
-
-// type Link {
-//     id: ID!
-//     description: String!
-//     url: String!
-// }
-// `
-// our data structure to store the data for links
-// let links = [{
-//     id: 'link-0',
-//     url: 'www.howtographql.com',
-//     description: 'Setting up resolvers'
-// }]
-
-// let idCount = links.length
 
 const resolvers = {
-    Query: {
-        info: () => `McEEN Media`,
-        feed: (root, args, context, info) => {
-            return context.prisma.links()
-        },
-    },
-
-    Mutation: {
-        post: (root, args, context) => {
-            return context.prisma.createLink({
-                url: args.url,
-                description: args.description,
-            })
-        },
-    },
-}
+    Query,
+    Mutation,
+    User,
+    Link
+  }
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
-    context: { prisma },
+    context: request => { 
+        return {
+            ...request,
+            prisma
+        }
+    },
 })
 
 server.start(() => console.log(`Server started on http://localhost:4000`));
